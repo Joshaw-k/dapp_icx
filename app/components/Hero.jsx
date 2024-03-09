84b40263867cf962c3f2b012455917bdeb107f63
Joshaw-k <awinrin4bless@gmail.com>
2024-03-09 23:12:59 +0100

    first commit

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -13,10 +13,10 @@ const Hero = () => {
             This is not an app but a protocol that establishes a remote
             resolution between all noncustodial wallet.
           </h4>
-          <button className="connect-wallet w-fit py-1.5 px-3 rounded-full text-white font-medium bg-gradient-to-r from-[#577BF8] to-[#A991D8] hover:bg-gradient-to-l">
+          {/* <button className="connect-wallet w-fit py-1.5 px-3 rounded-full text-white font-medium bg-gradient-to-r from-[#577BF8] to-[#A991D8] hover:bg-gradient-to-l">
             Connect Wallet
-          </button>
-          {/* <w3m-button label="Connect Wallet" /> */}
+          </button> */}
+          <w3m-button label="Connect Wallet" />
         </div>
         <div>
           <img src="/cta-img-with-bg.png" className="sm:scale-90" alt="" />
